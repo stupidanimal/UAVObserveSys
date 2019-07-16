44ac29756313e5dc6d0cbf4731dbdf31d19f3dce
‘stupidanimal’ <stupidanimal@outlook.com>
2019-07-16 17:05:10 +0800

    添加其中一个客户端

---
+++ webclient/SystemUI2/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>
+        <el-menu class="el-menu-demo" mode="horizontal" router>
+          <el-menu-item>
+            <el-image src="../public/Icon.png"></el-image>
+          </el-menu-item>
+          <el-menu-item index="/map">Map</el-menu-item>
+          <el-menu-item index="/page1">Menu1</el-menu-item>
+          <el-menu-item index="/page2">Menu2</el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+      <el-footer>footera</el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  components: {}
+};
+</script>
+
+<style scoped>
+#app {
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 auto;
+}
+</style>
---
+++ webclient/SystemUI2/src/pages/Map.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-row>
+    <el-col>
+      <div class="block" v-bind:style="{height:style.height+'px'}">
+        <MapComp></MapComp>
+      </div>
+    </el-col>
+  </el-row>
+</template>
+<script>
+import MapComp from "./components/MapComp";
+export default {
+  data() {
+    return {
+      style: {
+        height: 0
+      }
+    };
+  },
+  components: {
+    MapComp
+  },
+  mounted() {
+    let height = window.innerHeight;
+    this.style.height = Math.ceil(height * 0.75);
+  }
+};
+</script>
+<style scoped>
+.pageContainer {
+  display: block;
+  background: pink;
+}
+.block {
+  height: 80%;
+  background: pink;
+}
+</style>
+
+
