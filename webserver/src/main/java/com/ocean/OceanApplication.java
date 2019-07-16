c113ad73cfa8acf7320b9376e609af02f07f88e1
seriousSam <stupidanimal@outlook.com>
2019-07-17 02:09:50 +0800

    添加接口 添加跨域

--- webserver/src/main/java/com/ocean/OceanApplication.java
+++ webserver/src/main/java/com/ocean/OceanApplication.java
@@ -3,12 +3,18 @@ package com.ocean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * @author xingzhe
  * @description
  * @update 2014年6月20日 上午10:48:42
  */
+
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class OceanApplication {
 
